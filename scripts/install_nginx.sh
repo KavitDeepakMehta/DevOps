58016f9ef86090433c9117dd31ab5484e18fb13f
Kavit Mehta <39302323+KavitDeepakMehta@users.noreply.github.com>
2024-09-26 02:58:21 +0530

    Add files via upload

---
+++ scripts/install_nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update -y
+
+# Install nginx
+sudo apt-get install -y nginx
---
+++ scripts/start_nginx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Start nginx service
+sudo service nginx start
